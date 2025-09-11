928346b5766dc0a5de0eea95d9377cd8ce4ca17b
Edunardi08 <edunardi1208@gmail.com>
2025-09-10 22:12:35 -0300

    Create ex-01.js

---
+++ lista-pa/ex-01.js
@@ -0,0 +1,9 @@
+let num1 = input.questionInt("digite um numero: ")
+let num2 = input.questionInt("digite um numero: ")
+
+if (num1 > num2) {
+  console.log("O maior número é: " + num1);
+}
+ else {
+  console.log("O maior número é: " + num2);
+}
