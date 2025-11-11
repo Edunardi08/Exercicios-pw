33bd78564bb7e80eb3e7d5a8fd3fff1633e716d1
Edunardi08 <edunardi1208@gmail.com>
2025-11-10 21:12:50 -0300

    Delete lista-pa directory

--- lista-pa/ex-01.js
+++
@@ -1,9 +0,0 @@
-let num1 = input.questionInt("digite um numero: ")
-let num2 = input.questionInt("digite um numero: ")
-
-if (num1 > num2) {
-  console.log("O maior número é: " + num1);
-}
- else {
-  console.log("O maior número é: " + num2);
-}
